2929090a4398562aa6d8690913b609b9398fae45
ramandevops789 <ramana.linux789@gmail.com>
2025-01-21 13:21:50 +0530

    terraform

---
+++ expense/variables.tf
@@ -0,0 +1,21 @@
+variable "instance_names" {
+    type = list(string)
+    default = ["mysql", "backend", "frontend"]
+}
+
+variable "common_tags" {
+    type = map
+    default = {
+        Project = "expense"
+        Environment = "dev"
+        Terraform = "true"
+    }
+}
+
+variable "zone_id" {
+    default = "Z03242282DPN9FEAGOP0O"
+}
+
+variable "domain_name" {
+    default = "devopsaws.online"
+}
